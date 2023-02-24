199f6df10154d7a04519993623e8d0cc3b711722
Two-Jay <djeeee1272@gmail.com>
2023-02-25 02:45:15 +0900

    ex01 done

--- ex01/exec.py
+++ ex01/exec.py
@@ -1,7 +1,24 @@
 import argparse
 
-def main():
-    print('hello')
+def convert(arg):
+    ret = ''
+    for i in range(len(arg)):
+        if arg[i].islower():
+            ret += arg[i].upper()
+        elif arg[i].isupper():
+            ret += arg[i].lower()
+        else:
+            ret += arg[i]
+    return ret[::-1]
+
+def main(args):
+    print(' '.join(map(convert, args)))
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(nargs = '*', dest = 'arg')
+    args = parser.parse_args().arg
+    return args
 
 if __name__ == "__main__":
-    main()
+    main(get_args()[::-1])
