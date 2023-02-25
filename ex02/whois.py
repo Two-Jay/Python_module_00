5f1ede27e75b5c138fafc2cef1c6c9ff0a266c69
Two-Jay <djeeee1272@gmail.com>
2023-02-25 12:32:37 +0900

    ex02 fin

--- ex02/whois.py
+++ ex02/whois.py
@@ -1,6 +1,34 @@
+import argparse
+import sys
 
-def main():
-    print()
+sys.tracebacklimit = 0
+
+errmsg_argument_maximum = 'more than one argument are provided'
+errmsg_argument_type = 'argument is not an integer'
+
+EVEN = 'Even'
+ODD = 'Odd'
+ZERO = 'Zero'
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(nargs = '*', dest = 'arg')
+    args = parser.parse_args().arg
+    return args
+
+def validate_args(args):
+    assert len(args) <= 1, errmsg_argument_maximum
+    assert args[0].isdigit(), errmsg_argument_type if len(args) == 1 else None
+
+def check_answer(args):
+    return ZERO if int(args[0]) == 0 else EVEN if int(args[0]) % 2 == 0 else ODD
+
+def print_answer(result : str):
+    print(f'I\'m {result}.')
+
+def main(args):
+    validate_args(args)
+    print_answer(check_answer(args))
 
 if __name__ == "__main__":
-    main()
+    main(get_args())
