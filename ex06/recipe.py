235b98dd8a02369ceb94660b4abbd90c22c6b583
Two-Jay <djeeee1272@gmail.com>
2023-02-26 00:05:06 +0900

    ex06 comp

--- ex06/recipe.py
+++ ex06/recipe.py
@@ -16,7 +16,11 @@ cookbook = {
     }
 }
 
-def print_recipe(recipe_name):
+def print_recipe():
+    recipe_name = input('Enter a recipe name: ')
+    print_recipe_impl(recipe_name)
+
+def print_recipe_impl(recipe_name):
     if recipe_name in cookbook:
         print(f"Recipe for {recipe_name}:")
         print(f"  Ingredients list: {cookbook[recipe_name]['ingredients']}")
@@ -30,20 +34,67 @@ def print_cookbook():
     for key, value in cookbook.items():
         print(f"  - {key}")
 
-def add_recipe(recipe_name, ingredients, meal, prep_time):
+def add_recipe_impl(recipe_name, ingredients, meal, prep_time):
     cookbook[recipe_name] = {
         'ingredients': ingredients,
         'meal': meal,
         'prep_time': prep_time
     }
 
-def delete_recipe(recipe_name):
+def get_input_ingrediants():
+    print('Enter an ingredients: ')
+    ingredients = []
+    while True:
+        ingredient = input()
+        if ingredient == '':
+            break
+        ingredients.append(ingredient)
+    return ingredients
+
+def add_recipe():
+    recipe_name = input('Enter a name: ')
+    ingredients = get_input_ingrediants()
+    meal = input('Enter a meal type: ')
+    prep_time = input('Enter a preparation time in minutes: ')
+    add_recipe_impl(recipe_name, ingredients, meal, prep_time)
+
+def delete_recipe():
+    recipe_name = input('Enter a recipe name: ')
+    delete_recipe_impl(recipe_name)
+
+def delete_recipe_impl(recipe_name):
     if recipe_name in cookbook:
         del cookbook[recipe_name]
     else:
         print('This recipe does not exist')
 
+def print_header():
+    print("Welcome to the Python Cookbook !\nList available options:")
+    for key, value in cookBook_prompt_options.items():
+        print(f"  {key}: {value[0]}")
+
+def quit():
+    print('Cookbook closed. Goodbye!')
+    exit()
+
+cookBook_prompt_options = {
+    '1': ['Add a recipe', add_recipe],
+    '2': ['Delete a recipe', delete_recipe],
+    '3': ['Print a recipe', print_recipe],
+    '4': ['Print the cookbook', print_cookbook],
+    '5': ['Quit', quit]
+}
+
+def main():
+    print_header()
+    while True:
+        choice = input('Please select an option: ')
+        if choice in cookBook_prompt_options:
+            function = cookBook_prompt_options[choice][1]
+            function()
+        else:
+            print('This option does not exist, please type the corresponding number.')
+
 if __name__ == "__main__":
-    add_recipe('pizza', ['cheese', 'tomatoes', 'ham'], 'lunch', 30)
-    print_cookbook()
-    print_recipe('pizza')
+    main()
+
