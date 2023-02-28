58f3fa7f49b49e961885a09ad89083f7cb25b9eb
Two-Jay <djeeee1272@gmail.com>
2023-03-01 03:10:25 +0900

    kata comp

--- ex05/kata03.py
+++ ex05/kata03.py
@@ -1,7 +1,14 @@
+# * kata value
+# --------------------------------------------------------------------------
 kata = "The right format"
 
-def main():
+# * main
+# --------------------------------------------------------------------------
+def do_kata(kata : str) -> None:
     print(f"{kata:->42}", end='')
+
+def main():
+    do_kata(kata)
 
 if __name__ == "__main__":
     main()
