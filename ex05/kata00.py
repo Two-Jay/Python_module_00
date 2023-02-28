8e6bd4d4888d5d6cb5b617c6c6d5a99330422082
Two-Jay <djeeee1272@gmail.com>
2023-03-01 01:52:41 +0900

    edit kata00

--- ex05/kata00.py
+++ ex05/kata00.py
@@ -1,7 +1,13 @@
 kata = (19,42,21)
 
+def do_kata(kata : tuple) -> None:
+    print(
+        f'The 3 numbers are:',
+        ' '.join([f'{i}' for i in kata])
+    )
+
 def main():
-    print(f"The 3 numbers are: {kata[0]}, {kata[1]}, {kata[2]}")
+    do_kata(kata)
 
 if __name__ == "__main__":
     main()
