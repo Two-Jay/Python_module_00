8866ac1ff1805edf25df699dea1a0665a1decb98
Two-Jay <djeeee1272@gmail.com>
2023-04-09 17:55:05 +0900

    kata00 validate added

--- ex05/kata00.py
+++ ex05/kata00.py
@@ -1,13 +1,15 @@
 kata = (19,42,21)
 
+def validate(kata : tuple) -> bool:
+    return isinstance(kata, tuple) and len(kata) == 3 and all(type(x) == int for x in kata)
+
 def do_kata(kata : tuple) -> None:
-    print(
-        f'The 3 numbers are:',
-        ' '.join([f'{i}' for i in kata])
-    )
+    if validate(kata):
+        print(f"The 3 numbers are:{' '.join([f'{i}' for i in kata])}")
 
 def main():
     do_kata(kata)
+    do_kata(kata2)
 
 if __name__ == "__main__":
     main()
