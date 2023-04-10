70e76ad6a347d007d584f00bd70103ad6654f3c7
Two-Jay <djeeee1272@gmail.com>
2023-04-10 11:19:46 +0900

    kata01 validate added

--- ex05/kata01.py
+++ ex05/kata01.py
@@ -8,9 +8,13 @@ kata = {
 
 # * main
 # --------------------------------------------------------------------------
-def do_kata(kata : dict)
-    for key, value in kata.items():
-        print(f"{key} was created by {value}")
+def validate(kata : dict) -> bool:
+    return isinstance(kata, dict) and all(type(x) == str for x in kata.values()) and all(type(x) == str for x in kata.keys())
+
+def do_kata(kata : dict):
+    if validate(kata):
+        for key, value in kata.items():
+            print(f"{key} was created by {value}")
 
 def main():
     do_kata(kata)
